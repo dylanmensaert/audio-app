aec464ca6b0da9840656d6bb9aef03999da66e24
dylanmensaert <dylan.mensaert@gmail.com>
2015-08-18 18:27:38 +0200

    Modified mdl-spinner. Fixed mdl-header.

--- app/components/mdl-header.js
+++ app/components/mdl-header.js
@@ -2,8 +2,14 @@
 import MyMdlComponent from 'audio-app/components/my-mdl';
 
 export default MyMdlComponent.extend({
+    didInsertElement: function () {
+        this._super();
+
+        componentHandler.upgradeElement(this.$().closest('.mdl-layout')[0]);
+    },
     willDestroyElement: function () {
-        // TODO: Uncaught Error: Invalid argument provided to downgrade MDL nodes.
-        componentHandler.downgradeElements(this.$().closest('.mdl-layout')[0]);
+        // TODO: https://github.com/google/material-design-lite/issues/1340
+        // componentHandler.downgradeElements(this.$().closest('.mdl-layout')[0]);
+        this.$().closest('.mdl-layout').removeAttr('data-upgraded');
     }
 });
