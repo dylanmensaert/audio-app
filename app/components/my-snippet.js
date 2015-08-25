4eee12d19f20d43f1a59418e1f3010d68b9ff994
dylanmensaert <dylan.mensaert@gmail.com>
2015-08-25 23:07:20 +0200

    Implemented snippets as cards

--- app/components/my-snippet.js
+++ app/components/my-snippet.js
@@ -1,34 +1,37 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  layoutName: 'snippet',
-  classNames: ['list-group-item', 'snippet-item'],
-  classNameBindings: ['model.isSelected:btn-material-grey-400'],
-  attributeBindings: ['name'],
-  model: null,
-  name: function() {
-      return this.get('model.id');
-  }.property('model.id'),
-  showQueued: false,
-  didInsertElement: function() {
-      this.$().on('swipeleft', function() {
-          this.sendAction('swipeleft', this.get('model'));
-      }.bind(this));
+    layoutName: 'snippet',
+    classNames: ['mdl-card', 'mdl-cell', 'mdl-shadow--2dp', 'my-card'],
+    classNameBindings: ['model.isSelected:active'],
+    attributeBindings: ['name'],
+    model: null,
+    name: function () {
+        return this.get('model.id');
+    }.property('model.id'),
+    showQueued: false,
+    didInsertElement: function () {
+        this.$().on('swipeleft', function () {
+            this.sendAction('swipeleft', this.get('model'));
+        }.bind(this));
 
-      this.$().on('swiperight', function() {
-          this.sendAction('swiperight', this.get('model'));
-      }.bind(this));
-  },
-  willDestroyElement: function() {
-      this.$().off('swipeleft');
-      this.$().off('swiperight');
-  },
-  actions: {
-      toggleSelection: function() {
-          this.get('model').toggleProperty('isSelected');
-      },
-      click: function() {
-          this.sendAction('action', this.get('model'));
-      }
-  }
+        this.$().on('swiperight', function () {
+            this.sendAction('swiperight', this.get('model'));
+        }.bind(this));
+    },
+    willDestroyElement: function () {
+        this.$().off('swipeleft');
+        this.$().off('swiperight');
+    },
+    hasStatus: function () {
+        return this.get('model.isPlaying') || this.get('showQueued') || this.get('model.isDownloading') || this.get('model.isDownloaded');
+    }.property('model.isPlaying', 'showQueued', 'model.isDownloading', 'model.isDownloaded'),
+    actions: {
+        toggleSelection: function () {
+            this.get('model').toggleProperty('isSelected');
+        },
+        click: function () {
+            this.sendAction('action', this.get('model'));
+        }
+    }
 });
