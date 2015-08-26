dfcb2a1a047a3be29650d8867f98b9a5b39654ca
dylanmensaert <dylan.mensaert@gmail.com>
2015-08-26 23:15:46 +0200

    Implemented cards with dynamic images.

--- app/components/my-snippet.js
+++ app/components/my-snippet.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     layoutName: 'snippet',
-    classNames: ['mdl-card', 'mdl-cell', 'mdl-shadow--2dp', 'my-card'],
+    classNames: ['mdl-card', 'mdl-cell', 'mdl-shadow--2dp', 'my-card', 'mdl-cell--2-col-phone', 'mdl-cell--4-col-tablet'],
     classNameBindings: ['model.isSelected:active'],
     attributeBindings: ['name'],
     model: null,
@@ -11,6 +11,9 @@ export default Ember.Component.extend({
     }.property('model.id'),
     showQueued: false,
     didInsertElement: function () {
+        var outerImage = this.$('.outer-image'),
+            innerImage = this.$('.inner-image');
+
         this.$().on('swipeleft', function () {
             this.sendAction('swipeleft', this.get('model'));
         }.bind(this));
@@ -18,6 +21,11 @@ export default Ember.Component.extend({
         this.$().on('swiperight', function () {
             this.sendAction('swiperight', this.get('model'));
         }.bind(this));
+
+        // TODO: duplicate with mdl-layout/component
+        outerImage.height(outerImage.width() / 30 * 17);
+        innerImage.height(innerImage.width() / 12 * 9);
+        innerImage.css('top', -Math.floor((innerImage.height() - outerImage.height()) / 2));
     },
     willDestroyElement: function () {
         this.$().off('swipeleft');
--- app/mdl-layout/component.js
+++ app/mdl-layout/component.js
@@ -1,5 +1,23 @@
+import Ember from 'ember';
 import MyMdlComponent from 'audio-app/components/my-mdl';
 
 export default MyMdlComponent.extend({
-    classNames: ['mdl-layout', 'mdl-js-layout', 'mdl-layout--fixed-drawer', 'mdl-layout--fixed-header']
+    classNames: ['mdl-layout', 'mdl-js-layout', 'mdl-layout--fixed-drawer', 'mdl-layout--fixed-header'],
+    didInsertElement: function () {
+        Ember.$(window).resize(function () {
+            // TODO: duplicate with my-snippet/component
+            this.$('.outer-image').each(function () {
+                Ember.$(this).height(Ember.$(this).width() / 30 * 17);
+            });
+
+            this.$('.inner-image').each(function () {
+                Ember.$(this).height(Ember.$(this).width() / 12 * 9);
+
+                Ember.$(this).css('top', -Math.floor((Ember.$(this).height() - Ember.$(this).parent().height()) / 2));
+            });
+        }.bind(this));
+    },
+    willDestroyElement: function () {
+        Ember.$(window).off('resize');
+    }
 });
