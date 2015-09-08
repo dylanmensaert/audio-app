5d1cbe603c8750f195318515c277085711f37971
dylanmensaert <dylan.mensaert@gmail.com>
2015-09-08 23:19:31 +0200

    Fixed bug

--- app/application/controller.js
+++ app/application/controller.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    cache: Ember.inject.service(),
     audioPlayer: Ember.inject.service(),
     isLoading: false,
     linkToProperties: {
