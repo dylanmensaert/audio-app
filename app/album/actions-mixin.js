c9e90f8707d030767ee4d6c4a0f7e7f80e85950b
dylanmensaert <dylan.mensaert@gmail.com>
2015-09-21 18:23:36 +0200

    Implemented actions

--- app/album/actions-mixin.js
+++ app/album/actions-mixin.js
@@ -3,25 +3,29 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
     editedAlbumName: null,
     actions: {
-        save: function() {
-
+        save: function () {
+            this.get('selectedAlbums').forEach(function (album) {
+                album.save();
+            });
         },
-        delete: function() {
-
+        delete: function () {
+            this.get('selectedAlbums').forEach(function (album) {
+                album.destroyRecord();
+            });
         },
-        setupEdit: function() {
+        setupEdit: function () {
             var name = this.get('selectedAlbums.firstObject.name');
 
             this.set('editedAlbumName', name);
         },
-        saveEdit: function() {
+        saveEdit: function () {
             var selectedAlbum = this.get('selectedAlbums.firstObject');
 
             selectedAlbum.set('name', this.get('editedAlbumName'));
 
             this.set('editedAlbumName', null);
         },
-        exitEdit: function() {
+        exitEdit: function () {
             this.set('editedAlbumName', null);
         }
     }
