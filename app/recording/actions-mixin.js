2a62f548d427d029039235b5b47fb0fd858d7c03
dylanmensaert <dylan.mensaert@gmail.com>
2015-09-22 22:23:43 +0200

    Fixed some old code

--- app/recording/actions-mixin.js
+++ app/recording/actions-mixin.js
@@ -3,45 +3,54 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
     cache: Ember.inject.service(),
     actions: {
-        download: function() {
-            // TODO: use cache.selectedSnippets instead? Not sure since also used to pass album to route at the moment.
-            this.get('selectedRecordings').forEach(function(recording) {
+        download: function () {
+            this.get('selectedRecordings').forEach(function (recording) {
                 if (!recording.get('isDownloaded') && !recording.get('isDownloading')) {
                     recording.download();
                 }
             });
         },
-        pushToDownload: function(recording) {
-            var cache = this.get('cache');
+        pushToDownload: function (recording) {
+            var cache = this.get('cache'),
+                recordingIds,
+                id;
 
-            if (!recording.get('isDownloaded')) {
+            if (!recording.get('isDownloaded') && !recording.get('isDownloading')) {
                 if (!cache.isMobileConnection()) {
-                    recording.download().then(function() {
+                    recording.download().then(function () {
 
-                    }, function() {
+                    }, function () {
                         // TODO: show error?
                         cache.showMessage('download aborted');
                     });
                 } else {
-                    this.get('fileSystem.albums').findBy('name', 'Download later').get('recordingIds').pushObject(recording.get('id'));
+                    recordingIds = this.get('store').peekRecord('album', 'download-later').get('recordingIds');
+                    id = recording.get('id');
+
+                    if (!recordingIds.contains(id)) {
+                        recordingIds.pushObject(id);
+                    }
+
+                    cache.showMessage('Added to album: Download later');
                 }
             } else {
                 cache.showMessage('already downloaded');
             }
         },
-        pushToQueue: function(recording) {
-            var queue = this.get('fileSystem.albums').findBy('name', 'Queue').get('recordingIds'),
-                cache = this.get('cache');
+        pushToQueue: function (recording) {
+            var recordingIds = this.get('store').peekRecord('album', 'queue').get('recordingIds'),
+                cache = this.get('cache'),
+                id = recording.get('id');
 
-            if (!queue.contains(recording.get('id'))) {
-                if (!recording.get('isDownloaded')) {
-                    recording.download().then(function() {}, function() {
+            if (!recordingIds.contains(id)) {
+                if (!recording.get('isDownloaded') && !recording.get('isDownloading')) {
+                    recording.download().then(function () {}, function () {
                         // TODO: show error?
                         cache.showMessage('Download aborted');
                     }.bind(this));
                 }
 
-                this.get('fileSystem.queue').pushObject(recording.get('id'));
+                recordingIds.pushObject(id);
 
                 cache.showMessage('Added to queue');
             } else {
