8b2eb088e7ce36d680da0b30972dc1b13464a710
dylanmensaert <dylan.mensaert@gmail.com>
2015-09-24 12:53:55 +0200

    Fix

--- app/components/c-recordings-bar/component.js
+++ app/components/c-recordings-bar/component.js
@@ -1,41 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    cache: Ember.inject.service(),
     classNames: ['action-bar'],
     recordings: null,
-    undownloadedRecordings: function() {
-        return this.get('recordings').filter(function(recording) {
+    undownloadedRecordings: function () {
+        return this.get('recordings').filter(function (recording) {
             return !recording.get('isDownloaded') && !recording.get('isDownloading');
         });
     }.property('recordings.@each.isDownloaded'),
-    isEveryUndownloaded: function() {
+    isEveryUndownloaded: function () {
         return this.get('undownloadedRecordings.length') === this.get('recordings.length');
     }.property('undownloadedRecordings.length', 'recordings.length'),
-    downloadedRecordings: function() {
-        return this.get('recordings').filter(function(recording) {
+    downloadedRecordings: function () {
+        return this.get('recordings').filter(function (recording) {
             return recording.get('isDownloaded') || recording.get('isDownloading');
         });
     }.property('recordings.@each.isDownloaded'),
-    isEveryDownloaded: function() {
+    isEveryDownloaded: function () {
         return this.get('downloadedRecordings.length') === this.get('recordings.length');
     }.property('downloadedRecordings.length', 'recordings.length'),
-    unQueuedRecordings: function() {
+    unQueuedRecordings: function () {
         return this.get('recordings').filterBy('isQueued', false);
     }.property('recordings.@each.isQueued'),
-    isEveryUnQueued: function() {
+    isEveryUnQueued: function () {
         return this.get('unQueuedRecordings.length') === this.get('recordings.length');
     }.property('unQueuedRecordings.length', 'recordings.length'),
     actions: {
-        download: function() {
+        download: function () {
             this.sendAction('download');
         },
-        delete: function() {
+        delete: function () {
             this.sendAction('delete');
         },
-        queue: function() {
+        queue: function () {
             this.sendAction('queue');
         },
-        transitionToAlbums: function() {
+        transitionToAlbums: function () {
             this.set('cache.selectedSnippetIds', this.get('recordings').mapBy('id'));
 
             // TODO: implement transitionToAlbums
