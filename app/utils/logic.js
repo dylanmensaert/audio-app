6551025b6dc691a43a51fc516039a94162a2e88c
dylanmensaert <dylan.mensaert@gmail.com>
2015-10-25 23:35:41 +0100

    Started implementation for fix

--- app/utils/logic.js
+++ app/utils/logic.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 function isMatch(value, query) {
-    return query.trim().split(' ').every(function (queryPart) {
+    return query.trim().split(' ').every(function(queryPart) {
         return value.toLowerCase().includes(queryPart.toLowerCase());
     });
 }
@@ -10,7 +10,7 @@ function isMatch(value, query) {
 // TODO: rename logic file?
 export default {
     isMatch: isMatch,
-    generateRandomId: function () {
+    generateRandomId: function() {
         var randomId = '',
             possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
             index = 0;
@@ -21,32 +21,32 @@ export default {
 
         return randomId;
     },
-    getTopRecords: function (records, limit) {
+    getTopRecords: function(records, limit) {
         var topRecords = [];
 
-        records.any(function (track, index) {
-            topRecords.pushObject(track);
+        records.any(function(record, index) {
+            topRecords.pushObject(record);
 
             return index + 1 >= limit;
         });
 
         return topRecords;
     },
-    find: function (modelName, options, searchOnline) {
+    find: function(modelName, options, searchOnline) {
         var store = this.get('store'),
             promise,
             promiseArray;
 
         if (!searchOnline) {
-            promise = new Ember.RSVP.Promise(function (resolve) {
+            promise = new Ember.RSVP.Promise(function(resolve) {
                 var snippets;
 
                 if (options.collectionId) {
-                    snippets = store.peekRecord('collection', options.collectionId).get('trackIds').map(function (trackId) {
+                    snippets = store.peekRecord('collection', options.collectionId).get('trackIds').map(function(trackId) {
                         return store.peekRecord('track', trackId);
                     });
                 } else {
-                    snippets = store.peekAll(modelName).filter(function (snippet) {
+                    snippets = store.peekAll(modelName).filter(function(snippet) {
                         return !snippet.get('permission') && isMatch(snippet.get('name'), options.query);
                     });
                 }
@@ -59,7 +59,7 @@ export default {
             });
 
         } else {
-            options.setNextPageToken = function (nextPageToken) {
+            options.setNextPageToken = function(nextPageToken) {
                 this.set('nextPageToken', nextPageToken);
             }.bind(this);
 
