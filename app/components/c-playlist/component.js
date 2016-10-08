64abadbef93ca7a56be1a5575456712d790d2f51
dylan.mensaert@gmail.com <dylan.mensaert@gmail.com>
2016-10-08 02:16:03 +0200

    Further changes.

--- app/components/c-playlist/component.js
+++ app/components/c-playlist/component.js
@@ -4,10 +4,5 @@ import modelMixin from 'audio-app/mixins/c-model';
 export default Ember.Component.extend(modelMixin, {
     // TODO: add placeholder left and right to row of cells that can help fix cell width layout
     classNames: ['card'],
-    classNameBindings: ['model.isSelected:my-active'],
-    actions: {
-        click: function() {
-            this.sendAction('action', 'playlist', this.get('model'));
-        }
-    }
+    classNameBindings: ['model.isSelected:my-active']
 });
