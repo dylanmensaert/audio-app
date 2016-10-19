fc1853f69270e4590d7dfd5351c807fb81e457ba
dylan.mensaert@gmail.com <dylan.mensaert@gmail.com>
2016-10-19 20:08:22 +0200

    Further changes.

--- app/track/related/route.js
+++ app/track/related/route.js
@@ -1,6 +1,7 @@
-import Ember from "ember";
+import Ember from 'ember';
+import registerRouteMixin from 'audio-app/mixins/route-register';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(registerRouteMixin, {
     setupController: function(controller, model) {
         this._super(controller, model);
 
