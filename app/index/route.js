e66934b4f652e82f4dc4e1dc1656ec12abcfb322
dylanmensaert <dylan.mensaert@gmail.com>
2016-12-08 19:23:58 +0100

    Further changes

--- app/index/route.js
+++ app/index/route.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 import clearRouteMixin from 'audio-app/mixins/route-clear';
 
-export default Ember.Route.extend(clearRouteMixin, {
-    utils: Ember.inject.service(),
-    beforeModel: function() {
-        if (!this.store.peekRecord('playlist', 'history').get('trackIds.length')) {
-            this.transitionTo('search.tracks');
-        }
-    }
-});
+export default Ember.Route.extend(clearRouteMixin);
