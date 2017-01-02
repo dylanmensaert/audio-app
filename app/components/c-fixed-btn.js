5960e88d359abc84d1ace357fee7188e47488572
dylanmensaert <dylan.mensaert@gmail.com>
2017-01-02 19:40:47 +0100

    Fixed action btn

--- app/components/c-fixed-btn.js
+++ app/components/c-fixed-btn.js
@@ -5,22 +5,28 @@ import scrollMixin from 'audio-app/mixins/c-scroll';
 function updatePosition() {
     let element = this.$(),
         className = 'my-fixed',
-        btn = element.find('.btn-floating:first-child')[0];
+        btn = element.find('> a'),
+        wasFixed = element.hasClass(className),
+        redraw;
+
+    redraw = function() {
+        btn.remove();
+        element.prepend(btn);
+    }
 
     element.removeClass(className);
 
-    btn.style.webkitAnimation = 'none';
-
-    if (Ember.$(window).scrollTop() + this.get('utils.menuHeight') > element.find('> a').offset().top) {
+    if (Ember.$(window).scrollTop() + this.get('utils.menuHeight') > btn.offset().top) {
         element.addClass(className);
-    }
 
-    setTimeout(function() {
-        btn.style.webkitAnimation = '';
-    }, 1);
+        if (!wasFixed) {
+            redraw();
+        }
+    } else if (wasFixed) {
+        redraw();
+    }
 }
 
-// TODO: add popup css
 export default Ember.Component.extend(safeStyleMixin, scrollMixin, {
     style: Ember.computed('audioPlayer.track', 'utils.audioHeight', function() {
         let bottom = 45;
