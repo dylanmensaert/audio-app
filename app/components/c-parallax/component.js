105d275f97736f08718e1b31e3440a95a7bf1360
dylan.mensaert@gmail.com <dylan.mensaert@gmail.com>
2017-01-05 21:45:15 +0100

    Further changes

--- app/components/c-parallax/component.js
+++ app/components/c-parallax/component.js
@@ -4,17 +4,19 @@ import scrollMixin from 'audio-app/mixins/c-scroll';
 function scrollBackGround() {
     let element = this.$(),
         topDifference = Ember.$(window).scrollTop() + this.get('utils.menuHeight') - element.offset().top,
-        height = element.height();
+        height = element.height(),
+        image = this.$('.my-image');
 
     if (0 <= topDifference && topDifference <= height) {
         let percentage = topDifference / height * 100;
 
-        this.$('.my-image').css('background-position-y', (50 - percentage) + '%');
+        image.css('background-position-y', (50 - percentage) + '%');
+    } else if (parseInt(image.css('background-position-y') !== 50)) {
+        image.css('background-position-y', 50 + '%');
     }
 }
 
 export default Ember.Component.extend(scrollMixin, {
-    classNames: ['my-parallax'],
     src: null,
     utils: Ember.inject.service(),
     didInsertElement: function() {
