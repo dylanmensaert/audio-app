8ed0ae42b98ae42c5f5936f8076b2474b071333e
dylanmensaert <dylan.mensaert@gmail.com>
2017-02-21 00:33:15 +0100

    Refactoring selected

--- app/components/c-playlist/component.js
+++ app/components/c-playlist/component.js
@@ -3,9 +3,6 @@ import modelMixin from 'audio-app/mixins/c-model';
 
 export default Ember.Component.extend(modelMixin, {
     classNames: ['card'],
-    classNameBindings: ['isSelected:my-active', 'isSubscribe:my-playlist-subscribe'],
-    isSubscribe: false,
-    isSelected: Ember.computed('model.isSelected', 'isSubscribe', function() {
-        return this.get('model.isSelected') && !this.get('isSubscribe');
-    })
+    classNameBindings: ['model.isSelected:my-active', 'isSubscribe:my-playlist-subscribe'],
+    isSubscribe: false
 });
