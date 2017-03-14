7da6f36394f31009193e7a6afcf83a9a7d28cade
dylanmensaert <dylan.mensaert@gmail.com>
2017-03-15 00:37:34 +0100

    Fix tabs

--- app/components/c-tabs/component.js
+++ app/components/c-tabs/component.js
@@ -6,7 +6,7 @@ export default FixedComponent.extend({
     // TODO: needs position: relative to make border-bottom show up.
     didInsertElement: function() {
         // TODO: not working in mobile because of hash
-        /*this.$('.tabs').tabs();*/
+        this.$('.tabs').tabs();
 
         if (this.get('alignmentAtStart') === false) {
             this.$().css('position', 'static');
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -32,8 +32,8 @@ module.exports = function(defaults) {
     // please specify an object with the list of modules as keys
     // along with the exports of each module as its value.
 
-    app.import('bower_components/materialize/bin/materialize.css');
-    app.import('bower_components/materialize/bin/materialize.js');
+    app.import('bower_components/materialize/dist/css/materialize.css');
+    app.import('bower_components/materialize/dist/js/materialize.js');
 
     app.import('bower_components/material-design-icons/iconfont/material-icons.css');
 
