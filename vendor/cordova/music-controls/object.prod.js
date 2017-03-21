de88cd2a1015423388b21f310abb13efe9b8a602
dylan.mensaert@gmail.com <dylan.mensaert@gmail.com>
2017-03-21 19:00:12 +0100

    Syntax fix.

--- vendor/cordova/music-controls/object.prod.js
+++ vendor/cordova/music-controls/object.prod.js
@@ -1,47 +1,46 @@
 /*global MusicControls*/
 
 (function() {
-        define('music-controls', ['ember', 'cordova'], function(Ember, cordova) {
-                'use strict';
+    define('music-controls', ['ember', 'cordova'], function(Ember, cordova) {
+        'use strict';
 
-                Ember = Ember.default;
-                cordova = cordova.default;
+        Ember = Ember.default;
+        cordova = cordova.default;
 
-                var musicControls = {};
+        var musicControls = {};
 
-                cordova.onDeviceReady.then(function() {
-                        musicControls.init = function(context, actions) {
-                            MusicControls.subscribe(function(action) {
-                                actions[action].call(context);
-                            });
-
-                            MusicControls.listen();
-                        };
-
-                        musicControls.play: function() {
-                            MusicControls.updateIsPlaying(true);
-                        };
-
-                        musicControls.pause: function() {
-                            MusicControls.updateIsPlaying(false);
-                        };
-
-                        musicControls.load: function(track) {
-                            MusicControls.create({
-                                track: track.get('name'),
-                                cover: track.get('thumbnail'),
-                                ticker: 'Now playing "' + track.get('name') + '"'
-                            });
-                        };
-
-                        musicControls.destroy: function() {
-                            MusicControls.destroy();
-                        };
-                    }
+        cordova.onDeviceReady.then(function() {
+            musicControls.init = function(context, actions) {
+                MusicControls.subscribe(function(action) {
+                    actions[action].call(context);
                 });
 
-            return {
-                'default': musicControls
+                MusicControls.listen();
+            };
+
+            musicControls.play: function() {
+                MusicControls.updateIsPlaying(true);
+            };
+
+            musicControls.pause: function() {
+                MusicControls.updateIsPlaying(false);
+            };
+
+            musicControls.load: function(track) {
+                MusicControls.create({
+                    track: track.get('name'),
+                    cover: track.get('thumbnail'),
+                    ticker: 'Now playing "' + track.get('name') + '"'
+                });
+            };
+
+            musicControls.destroy: function() {
+                MusicControls.destroy();
             };
         });
+
+        return {
+            'default': musicControls
+        };
+    });
 })();
