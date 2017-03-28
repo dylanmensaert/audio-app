904fc7c836bb202d8e28779919da0d068a66b0c5
dylan.mensaert@gmail.com <dylan.mensaert@gmail.com>
2017-03-28 18:42:00 +0200

    Fix didScrollToBottom

--- app/components/c-tracks/component.js
+++ app/components/c-tracks/component.js
@@ -4,5 +4,10 @@ import trackActionsMixin from 'audio-app/mixins/actions-track';
 export default Ember.Component.extend(trackActionsMixin, {
     playlist: null,
     hideDownloaded: false,
-    selectedTracks: Ember.computed.filterBy('models', 'isSelected', true)
+    selectedTracks: Ember.computed.filterBy('models', 'isSelected', true),
+    actions: {
+        didScrollToBottom: function() {
+            this.sendAction('didScrollToBottom');
+        }
+    }
 });
