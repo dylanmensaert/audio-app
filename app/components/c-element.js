961067621188fa24715af0694f0dfe9e5438512d
dylanmensaert <dylan.mensaert@gmail.com>
2017-04-15 01:39:41 +0200

    track transition fixes. Hammer fix.

--- app/components/c-element.js
+++ app/components/c-element.js
@@ -1,7 +1,13 @@
+/*global Hammer*/
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
     didInsertElement: function() {
         this.sendAction('didInsert');
+
+        new Hammer(this.$()[0]).on('tap', function() {
+            this.sendAction('tap');
+        }.bind(this));
     }
 });
